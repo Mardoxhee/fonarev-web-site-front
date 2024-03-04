8d89912d9791f614fbaa3bda36f795c724a039d8
Mardochée L <mar@Berrys-MacBook-Air.local>
2024-03-04 10:41:12 +0100

    setting projetc

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,20 @@
+"use client"
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from './../components/header'
 import Footer from './../components/footer'
 import { Poppins } from 'next/font/google';
+import { store } from './store'
+import { Provider } from 'react-redux'
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Fonarev RDC",
-  description: "Fonds national des réparations des victimes",
-};
+// export const metadata = {
+//   title: "Fonarev RDC",
+//   description: "Fonds national des réparations des victimes",
+// };
 
+// 
 const poppins = Poppins({
   subsets: ['latin'], // Specify desired font subsets (optional)
   weight: ['300', '400', '700', '200', '500', '600', '800'], // Specify desired font weights (optional)
@@ -18,13 +22,14 @@ const poppins = Poppins({
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-
-      <body className={poppins.className}>
-        <Header/>
-          {children}
-        <Footer/>
-        </body>
-    </html>
+    <Provider store={store}>
+      <html lang="en">
+        <body className={poppins.className}>
+          <Header/>
+            {children}
+          <Footer/>
+          </body>
+      </html>
+    </Provider>
   );
 }
