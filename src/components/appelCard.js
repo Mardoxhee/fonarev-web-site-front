20bcaab509da9600f6c9a1144d5dfbeff53cef88
Mardochée L <mar@mluvikis-macbook.local>
2024-03-25 22:46:07 +0100

    fix download doc

--- src/components/appelCard.js
+++ src/components/appelCard.js
@@ -3,18 +3,18 @@ import React from 'react'
 import styles from "./appelCard.module.scss"
 import { Icon } from '@iconify/react';
 
-const AppelCard = () => {
+const AppelCard = ({titre, expiration, reference, link}) => {
   return (
     <div className={styles.cardCont}>
             <div className={styles.imgContainer}></div>
             <div className={styles.textSide}>
 
-                <h2>Construction de 20 hopitaux lore lore lore lore lore lore lore lroe lorelorelorelorelorel</h2>
+                <h2> {titre} </h2>
                 <div className={styles.inContainer}>
-                    <h4 className={styles.location}><Icon icon="ion:location-outline" className={styles.icone} /> Kinshasa</h4>
+                    <h4 className={styles.location}><Icon icon="zondicons:book-reference" className={styles.icone} /> {reference}</h4>
                     <div className={styles.ctaFlexer}>
-                        <h4 className={styles.timeExp}><Icon icon="uim:calender" className={styles.icone} /> Date d'expiration  : Le 20 mars 2024</h4>
-                        <button><Icon icon="ep:document" className={styles.icone} /> Télécharger le PDF</button>
+                        <h4 className={styles.timeExp}><Icon icon="uim:calender" className={styles.icone} /> Date d'expiration  : {expiration}</h4>
+                        <a href={link} download><Icon icon="ep:document" className={styles.icone} /> Télécharger le PDF</a>
                     </div>
                   
                 </div>
