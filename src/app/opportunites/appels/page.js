5d8c5772d7e8e314567c8fe6de0b82e55aec5dc9
Mardochée L <mar@mluvikis-macbook.local>
2024-03-26 16:18:34 +0100

    fix appels d'offres

--- src/app/opportunites/appels/page.js
+++ src/app/opportunites/appels/page.js
@@ -7,7 +7,7 @@ const Appel = () => {
         <main className={styles.main}>
           <section className={styles.bannerContainer}>
               <h1>
-                  Appels d'offres
+                  Appels d'offres 
               </h1>
               <p>
                 Retrouvez tous les appels d'offre disponibles au FONAREV.
@@ -20,9 +20,9 @@ const Appel = () => {
             </div>
 
             <div className={styles.cardContainer}>
-                <AppelCard link = "/FONAREV-Avis-General-de-Passation-des-MarchEs.pdf" titre = "AVIS GÉNÉRAL DE PASSATION DES MARCHÉS" expiration = "--" reference = "--"/>
-                <AppelCard  link = "/Fonarev-AMI.pdf-MAKOBOLA.pdf" reference = "AMI N° 001/ DG FONAREV/ CPM/ DML/ 03- 2024" expiration = "9 Avril 2024 " titre= "Sélection et recrutement des Consultants 'Bureaux d'études techniques - Bureaux de contrôle et ed surveil lance', chargés des Études techniques et architecturales; de l'évaluation environnementale et sociale préliminaires,et du contrôle et de la surveillance des travaux de réhabilitation / construction d'infrastructures hospitalières, scolaires et professionnelles et de commodités, d'ouvrages d'art à entreprendre et à ériger par le FONAREV dans la localité de MAKOBOLA."/>
-                <AppelCard link="/FONAREV-AMI-BET-all-sites.pdf" reference = "AMI N° 002/ DG FONAREV/ CPM/ DML/ 03- 2024" expiration = "9 Avril 2024"  titre = "Selection et recrutement des Consultants 'Bureaux d'étude techniques - Bureaux de controle et de surveillance', chargés des études techniques et architecturales de l'évaluation environnementale et sociale préliminaires, et du contrôle et de la surveillance des travaux de réhabilitation / construction d'infrastructures hospitalières, scolaires et professionnelles et de commodité d'ouvrages d'art à entreprendre et à ériger par le FONAREV dans l'Ituri, le Nord Kivu, le Sud Kivu, le Haut Katanga, le Tanganyika, le Kongo Central, le Kasaï, le Sud Ubangi, et Kinshasa." />
+                <AppelCard isAvis = {true} bigTitle = "AVIS GÉNÉRAL DE PASSATION DES MARCHÉS"link = "/FONAREV-Avis-General-de-Passation-des-MarchEs.pdf" titre = "Le Fonds National des Réparations des Victimes de violences sexuelles liées aux conflits et des victimes des crimes contre la paix et la sécurité de l'humanité, 'FONAREV' en sigle, a obtenu du Gouvernement Congolais un financement conséquent pour engager ses activités, et se propose d'utiliser les fonds pour régler des acquisitions en fournitures et services non intellectuels (services physiques), en travaux et prestations intellectuelles devant être acquis dans sa mise en œuvre. Le FONAREV est principalement financé par le Gouvernement Congolais. Les projets d'acquisitions envisagés s'articulent sur les natures citées dans la pièce jointe" expiration = "Mardi, le 26 Mars 2024" reference = "--"/>
+                <AppelCard  bigTitle = "Appels d'offre" link = "/FONAREV-AMI-MAKOBOLA.pdf" reference = "AMI N° 001/ DG FONAREV/ CPM/ DML/ 03- 2024" expiration = "9 Avril 2024 " titre= "Sélection et recrutement des Consultants 'Bureaux d'études techniques - Bureaux de contrôle et ed surveil lance', chargés des Études techniques et architecturales; de l'évaluation environnementale et sociale préliminaires,et du contrôle et de la surveillance des travaux de réhabilitation / construction d'infrastructures hospitalières, scolaires et professionnelles et de commodités, d'ouvrages d'art à entreprendre et à ériger par le FONAREV dans la localité de MAKOBOLA."/>
+                <AppelCard bigTitle = "Appels d'offre" link="/FONAREV-Avis-appel-a-manifestion-BET-all-sites.pdf" reference = "AMI N° 002/ DG FONAREV/ CPM/ DML/ 03- 2024" expiration = "9 Avril 2024"  titre = "Selection et recrutement des Consultants 'Bureaux d'étude techniques - Bureaux de controle et de surveillance', chargés des études techniques et architecturales de l'évaluation environnementale et sociale préliminaires, et du contrôle et de la surveillance des travaux de réhabilitation / construction d'infrastructures hospitalières, scolaires et professionnelles et de commodité d'ouvrages d'art à entreprendre et à ériger par le FONAREV dans l'Ituri, le Nord Kivu, le Sud Kivu, le Haut Katanga, le Tanganyika, le Kongo Central, le Kasaï, le Sud Ubangi, et Kinshasa." />
 
 
             </div>
