52fe476346067e3e760a1cc9725629ad0d83ad08
mluviki <mardoxheeluviki@gmail.com>
2026-01-28 11:04:56 +0100

    publish offer

--- src/app/opportunites/appels/page.js
+++ src/app/opportunites/appels/page.js
@@ -35,6 +35,9 @@ const Appel = () => {
                 {/* <Suspense fallback={<div>Chargement...</div>}>
                   <AppelCard isAvis={false} bigTitle="..." />
                 </Suspense> */}
+                  <AppelCard variant="compact" isAvis = {true}  bigTitle = "AVIS D'APPEL D'OFFRES publié le 27 janvier 2026" link="/AMI-CAUSES-PROFONDES-DES-CONFLITS-ARMES-ET-NON-ARMES.pdf" reference = "AMI N°05-PI/DG FONAREV/CGPMP/PF/01-2026" expiration = "Le 26 février 2026" titre = "RECRUTEMENT DES CONSULTANTS INDIVIDUELS CHARGES D E L'ETUDE SUR LES CAUSES PROFONDES DES CONFLITS ARMES ET NON ARMES EN REPUBLIQUE DEMOCRATIQUE DU CONGO" />
+                  <AppelCard variant="compact" isAvis = {true}  bigTitle = "AVIS D'APPEL D'OFFRES publié le 27 janvier 2026" link="/AMI-FORMALISATION-DEL-ACCOMPAGNEMENT-ADMINISTRATIF.pdf" reference = "AMI N°06-PI/DG FONAREV/CGPMP/PF/01-2026" expiration = "Le 26 février 2026" titre = "RECRUTEMENT D'UN CONSULTANT CABINET CHARGE DE LA FORMALISATION, D E L'ACCOMPAGNEMENT ADMINISTRATIF, TECHNIQUE ET LOGISTIQUE DES ASSOCIATIONS DES VICTIMES DANS LES PROVINCES DE KINSHASA, DU KONGO CENTRAL, DU KASAI ET DU KASAI ORIENTAL" />
+
                 <AppelCard variant="compact" isAvis = {true}  bigTitle = "AVIS D'APPEL D'OFFRES publié le 08 janvier 2026" link="/AVIS%20D%27APPEL%20D%27OFFRES%20-%20RECRUTEMENT%20DES%20AGENCES%20DE%20VOYAGES.pdf" reference = "AAOI N° 01- S/DG FONAREV/CGPMP/PF/12-2025" expiration = "Le 09 février 2026" titre = "MARCHE DE RECRUTEMENT DES AGENCES DE VOYAGE POUR L’ORGANISATION DES DEPLACEMENTS PROFESSIONNELS DES AGENTS DU FONAREV" />
 
                 <AppelCard variant="compact" isAvis = {true}  bigTitle = "AVIS D'APPEL D'OFFRES publié le 14 janvier 2026" link="/AMI-CABINET-DAVOCATS.pdf" reference = "AMI N°02-PI/DG FONAREV/CGPMP/PF/01-2026" expiration = "Le 13 février 2026" titre = "RECRUTEMENT D'UN CABINET D'AVOCATS CONSEIL POUR LE FONAREV" />
