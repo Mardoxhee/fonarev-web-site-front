9564b29ac20b4f317c32c70f1a49019a9db076d4
Beni-01 <contactmampbeni@gmail.com>
2026-07-27 12:33:24 +0100

    feat:add change

--- src/app/a-propos-du-fonarev/page.js
+++ src/app/a-propos-du-fonarev/page.js
@@ -247,14 +247,7 @@ const About = () => {
             </h2>
           </div>
           <div className={styles.videoFrame}>
-            <iframe
-              src="https://www.youtube.com/embed/Q0cVcXR3U8M?si=ZkD6liuDPmpunGq7"
-              title="Identifier les victimes, l’accès à la justice et allouer les réparations : le cœur de notre mission."
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerPolicy="strict-origin-when-cross-origin"
-              allowFullScreen
-            />
+             <iframe src="https://www.youtube.com/embed/lS2RRCws0iQ?si=dSkk_UMx_S1EA9ug" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
           </div>
         </section>
 
